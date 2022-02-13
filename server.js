0d746bc090850f43de49122b69c4e4080b19deb0
farhanzeb95 <farhanzeb95@gmail.com>
2022-02-13 14:42:49 +0100

    fixed deployment issue

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname + '../tourist-resorts-web'));
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'./src/index.html'));});
+app.listen(process.env.PORT || 8080);
